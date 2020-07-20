47e8974236ec174dca7874373ee536b920317af3
Matt Johnson <mossmatters@gmail.com>
2020-07-19 22:01:21 -0500

    Move Plink stats to new script

--- GenotypesToPCA.sh
+++ GenotypesToPCA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##End of Select Varitants for GATK4
 
-reference=Ppyriforme-3728.supercontigs.fasta
-prefix=Physcomitrium-pyriforme
+reference=$1 #Ppyriforme-3728.supercontigs.fasta
+prefix=$2 #Physcomitrium-pyriforme
 #Make Samples list
 */*-g.vcf > samples.list
 # Combine and Jointly call GVCFs
@@ -13,17 +13,3 @@ time gatk SelectVariants -V "$prefix".cohort.unfiltered.vcf -R $reference -selec
 time gatk VariantFiltration -R $reference -V "$prefix".SNPall.vcf --filter-name "hardfilter" -O "$prefix".snp.filtered.vcf --filter-expression "QD < 5.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0"
 gatk SelectVariants -V "$prefix".snp.filtered.vcf -O "$prefix".snp.filtered.nocall.vcf --set-filtered-gt-to-nocall
 
-
-## plink commands
-
-## Need to edit VCF to not have a dot 
-bcftools annotate --set-id +"%CHROM:%POS" > "$prefix".snp.filtered.nocall.vcf > "$prefix".snp.filtered.nodot.vcf
-plink --vcf-filter --vcf "$prefix".snp.filtered.nodot.vcf --allow-extra-chr --recode  --make-bed --geno --const-fid --out "$prefix"
-#Alternate for deleting all SNPs with missing data: 
-#   plink --vcf-filter --vcf "$prefix".snp.filtered.nodot.vcf --allow-extra-chr --recode  --make-bed --geno 0 --const-fid --out "$prefix"
-plink --indep 50 5 2 --file "$prefix" --allow-extra-chr --out "$prefix"
-plink --extract "$prefix".prune.in --out "$prefix"_pruned --file "$prefix" --make-bed --allow-extra-chr --recode
-# Generate eigenvalues and loadings for 20 PCA axes
-plink --pca 20 --file "$prefix"_pruned --allow-extra-chr --out "$prefix"
-# Generate basic stats (heterozygosity, inbreeding coefficient, allele frequencies)
-plink --freq --het --ibc --file "$prefix"_pruned --allow-extra-chr -out "$prefix"_pruned
---
+++ plink_stats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Script for generating statistics from the VCF output of 
+prefix=$1
+
+#Set an ID for each SNP so they can be filtered by name
+bcftools annotate --set-id +"%CHROM:%POS" --file "$prefix".snp.filtered.nocall.vcf > "$prefix".snp.filtered.nodot.vcf
+# Filter out SNPs that didn't pass the filter
+plink --vcf-filter --vcf "$prefix".snp.filtered.nodot.vcf --allow-extra-chr --recode  --make-bed --geno --const-fid --out "$prefix"
+#Alternate for deleting all SNPs with missing data: 
+#   plink --vcf-filter --vcf "$prefix".snp.filtered.nodot.vcf --allow-extra-chr --recode  --make-bed --geno 0 --const-fid --out "$prefix"
+plink --indep 50 5 2 --file "$prefix" --allow-extra-chr --out "$prefix"
+plink --extract "$prefix".prune.in --out "$prefix"_pruned --file "$prefix" --make-bed --allow-extra-chr --recode
+# Generate eigenvalues and loadings for 20 PCA axes
+plink --pca 20 --file "$prefix"_pruned --allow-extra-chr --out "$prefix"
+# Generate basic stats (heterozygosity, inbreeding coefficient, allele frequencies)
+plink --freq --het --ibc --file "$prefix"_pruned --allow-extra-chr -out "$prefix"_pruned
