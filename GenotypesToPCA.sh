5e0d3dc8b8fd45e84a77412e4e8073c8c1058f44
lindsawi <59423239+lindsawi@users.noreply.github.com>
2021-06-18 14:57:42 -0500

    Update GenotypesToPCA.sh

--- GenotypesToPCA.sh
+++ GenotypesToPCA.sh
@@ -11,6 +11,6 @@ gatk CombineGVCFs -R $reference --variant samples.list --output "$prefix".cohort
 gatk GenotypeGVCFs -R $reference -V "$prefix".cohort.g.vcf -O "$prefix".cohort.unfiltered.vcf
 # Keep only SNPs passing a hard filter
 time gatk SelectVariants -V "$prefix".cohort.unfiltered.vcf -R $reference -select-type-to-include SNP -O "$prefix".SNPall.vcf
-time gatk VariantFiltration -R $reference -V "$prefix".SNPall.vcf --filter-name "hardfilter" -O "$prefix".snp.filtered.vcf --filter-expression "QD < 5.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0"
+time gatk VariantFiltration -R $reference -V "$prefix".SNPall.vcf --filter-name "hardfilter" -O "$prefix".snp.filtered.vcf --filter-expression "QD < 5.0 & FS > 60.0 & MQ < 40.0 & MQRankSum < -12.5 & ReadPosRankSum < -8.0"
 gatk SelectVariants -V "$prefix".snp.filtered.vcf -O "$prefix".snp.filtered.nocall.vcf --set-filtered-gt-to-nocall
 
